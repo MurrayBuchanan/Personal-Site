bbea36b7c47cf38bcc3462e485a217d989965342
murray <murray.buchanan.2022@uni.strath.ac.uk>
2024-04-01 14:44:37 +0100

    Added Svelte

---
+++ Site/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+
+<div class="personal-section">
+	<img src="profile-picture.jpg" alt="Murray Buchanan">
+	<h1>Murray Buchanan</h1>
+	<p>Flutter App Developer</p>
+
+	<div class="links">
+		<a href="https://kit.svelte.dev"><img src="email.jpg" alt="Email"></a>
+		<a href="https://kit.svelte.dev"><img src="linkedin.jpg" alt="Linkedin"></a>
+		<a href="https://kit.svelte.dev"><img src="github.jpg" alt="Github"></a>
+		<a href="https://kit.svelte.dev"><img src="instagram.jpg" alt="Instagram"></a>
+	</div>
+</div>
+
+<div class="projects">
+	<div class="links">
+		<a href="https://kit.svelte.dev"><img src="Svelte" alt="SvelteKit"></a>
+		<a href="https://kit.svelte.dev"><img src="Svelte" alt="SvelteKit"></a>
+	</div>
+</div>
